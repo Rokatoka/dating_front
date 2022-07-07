01928f66be2c2d4bb44ee980512e3760b5255fc9
Rollan Sharipov <rokatoka@MacBook-Pro-Rollan.local>
2022-07-07 10:32:17 +0600

    update api base url

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://159.65.194.148:1337/api',
+  baseURL: 'https://astro-it.kz/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
